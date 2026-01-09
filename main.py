1e71ce173b001758f3f3225d4b5bc914c04f8b8d
Carlos Gutierrez <84545@alumnos.utleon.edu.mx>
2026-01-08 19:11:01 -0600

    Mi primer commit

--- main.py
+++ main.py
@@ -1,9 +1,63 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
+
 @app.route('/')
 def home():
-    return "Hello, world"
+    titulo="IDGS-802-Flask"
+    lista= ['Raul','Pepe','Ana']
+    return render_template('index.html', titulo=titulo, lista=lista)
+
+@app.route('/formularios')
+def formularios():
+    return render_template('formularios.html')
+
+@app.route('/reportes')
+def reportes():
+    return render_template('reportes.html')
+
+@app.route('/index')
+def index():
+    return render_template(index.html)
+
+@app.route('/hola')
+def hola():
+    return "Jola, Jola"
+
+@app.route('/user/<string:user>')
+def user(user):
+    return f("formulario")
+
+@app.route('/numero/<int:n>')
+def numero(n):
+    return "Numero: {}" .format(n)
+
+@app.route('/user/<int:id>/<string:username>')
+def username(id, username):
+    return "ID: {} nombre: {}" .format(id, username)
+
+@app.route('/suma/<float:n1>/<float:n2>')
+def func(n1, n2):
+    return "La suma es: {}" .format(n1+n2)
+
+@app.route('/default/')
+@app.route('/default/<string:param>')
+def func2(param='Juan'):
+    return f"<h1>Hola, {param}<h1>"
+
+@app.route('/operas')
+def operas():
+    return '''
+        <form>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+
+        <label for="name">Apaterno:</label>
+        <input type="text" id="name" name="name" required>
+        </form>
+'''
+
+
 if __name__=='__main__':
     app.run(debug=True)
 
